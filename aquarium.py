3b85f544c5c9e23bd7a83d5e371f6c78918d1e27
Tomer <tomer.shenhav@gmail.com>
2022-05-14 18:50:27 +0300

    saturday fun

--- aquarium.py
+++ aquarium.py
@@ -1,17 +1,62 @@
-from fish import Fish
+from mosquito import Mosquito
 
 
 class Aquarium:
-    l_fish = []
+    l_mosquito = []
+    total_male_gene = 0
+    total_male = 0
+    total_female = 0
+
+    def __init__(self):
+        self.l_mosquito = []
+        self.total_male_gene = 0
+        self.total_male = 0
+        self.total_female = 0
 
     def __str__(self):
-        mystr = 'My Aquarium\n===================='
-        for fish in self.l_fish:
-            mystr = mystr + '\n' + str(fish)
+        mystr = '====================\nMy Aquarium'
 
-        mystr = mystr + f'\nNumber od Fish in aquarium: {len(self.l_fish)}'
+        ratio = 0
+        if self.total_male > 0:
+            ratio = self.total_male_gene / self.total_male
+        mystr = mystr + f'\nNumber of mosquito in aquarium:' + str(len(self.l_mosquito)) + f'\nNumber of males:' + str(
+            self.total_male) + f'\nNumber of males with the "male gene":' + str(
+            self.total_male_gene) +' ('+ str('{0:.2f}'.format(ratio))+f') is the ration between males with to total males' + f'\nNumber of females:' + str(self.total_female)
+
         return mystr
 
-    def add_fish(self, amount_of_fish):
-        for i in range(amount_of_fish):
-            self.l_fish.append(Fish.random_fish())
+    def add_mosquito(self, amount_of_mosquito, male_gene_percetage):
+        for i in range(amount_of_mosquito):
+            self.l_mosquito.append(Mosquito.random_mosquito(male_gene_percetage))
+        self.total_male = 0
+        self.total_female = 0
+        self.total_male_gene = 0
+        self.update_counters(self.l_mosquito)
+
+    def update_counters(self,mylist):
+        for mos in mylist:
+            if mos.sex == 'XX':
+                self.total_female += 1
+            else:
+                if mos.male_gene == 'M':
+                    self.total_male_gene += 1
+                self.total_male += 1
+
+    def get_all_mosquito(self):
+        return self.l_mosquito
+
+    def get_total_mosquito(self):
+        return(len(self.l_mosquito))
+
+    def add_mosquitos_list(self, newborn_mosquito):
+        self.l_mosquito.extend(newborn_mosquito)
+        self.update_counters(newborn_mosquito)
+
+    def reduce_counters(self,mos):
+        if mos.sex == 'XX':
+            self.total_female -= 1
+        else:
+            if mos.male_gene == 'M':
+                self.total_male_gene -= 1
+            self.total_male -= 1
+
--- simulate.py
+++ simulate.py
@@ -1,7 +1,53 @@
+import datetime
+import random as rnd
 from aquarium import Aquarium
-from fish import Fish
+from mosquito import Mosquito
+import matplotlib.pyplot as plt
+import datetime
 
-aquarium = Aquarium()
-#print(aquarium)
-aquarium.add_fish(50)
-print(aquarium)
+
+def the_simulator(run):
+    day_counter = 1
+    # in days
+    simulation_length = 100
+    initial_mosquito = 1000
+    # the percentage of males with the "male gene"
+    male_gene_percentage = 50
+    # creating aquarium
+    aquarium = Aquarium()
+    aquarium.add_mosquito(initial_mosquito, male_gene_percentage)
+    print(aquarium)
+    # starting the simulation
+    while day_counter <= simulation_length and len(aquarium.get_all_mosquito()) != 0:
+        print("day number", day_counter)
+        day_counter_ls = []
+        plt.figure(run)
+        plt.plot(day_counter, aquarium.get_total_mosquito(), 'b.')
+        plt.plot(day_counter, aquarium.total_female, 'm.')
+        # loop through all mosquitoes in the aquarium and connect between them
+        mosquito_list = aquarium.get_all_mosquito()
+        newborn_mosquito = []
+        for mosquito in mosquito_list:
+            # print("mosquito:",mosquito)
+            newborn_mosquito = mosquito.meeting(rnd.choice(mosquito_list))
+            aquarium.add_mosquitos_list(newborn_mosquito)
+
+            mosquito.day_pass()
+            if mosquito.life_span == 0:
+                mosquito_list.remove(mosquito)
+                aquarium.reduce_counters(mosquito)
+        if day_counter % 5 == 0:
+            print(aquarium)
+
+        day_counter_ls.append(day_counter)
+        day_counter += 1
+    plt.xlabel('day number', fontsize=12)
+    plt.ylabel('total mosquitoes couter', fontsize=12)
+    plt.grid(True)
+    my_title = 'total mosquito (init =' + str(initial_mosquito), 'male gene per =' + str(male_gene_percentage)
+    plt.title(my_title, fontsize=18, color='blue')
+    date = datetime.datetime.now()
+    plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito) + '-per-' + str(male_gene_percentage) + '.png')
+
+for i in range(3):
+    the_simulator(i)
