d6f0b40e40aced96de159d311c017d607dcd7b04
Tomer <tomer.shenhav@gmail.com>
2022-08-22 18:49:29 +0300

    project final

--- drafts.py
+++ drafts.py
@@ -1,24 +1,35 @@
 import random
 import matplotlib.pyplot as plt
 
-x = range(1, 101)
-y1 = [random.randint(1, 100) for _ in range(len(x))]
-y2 = [random.randint(1, 100) for _ in range(len(x))]
 
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-ax2 = fig.add_subplot(212)
-
-ax1.loglog(x, y1)
-ax2.loglog(x, y2)
-
-# Set common labels
-#fig.text(0.5, 0.04, 'common xlabel', ha='center', va='center')
-#fig.text(0.06, 0.5, 'common ylabel', ha='center', va='center', rotation='vertical')
-
-ax1.set_title('ax1 title')
-ax1.set_xlabel('ax1knelk')
-ax2.set_title('ax2 title')
-ax2.set_xlabel('rgkjrnge')
-
+x=[14794, 173954, 920, 85808, 17337, 1546, 213733, 18697, 10077, 150985]
+plt.figure('summary')
+plt.hist(x, bins=[50000,100000,200000,300000,400000,500000,600000,700000] )
+plt.xlabel('day number', fontsize=12)
+plt.ylabel('amount of mosquitos', fontsize=12)
+my_title = 'test'
+plt.title(my_title)
+plt.grid(True)
 plt.show()
+#
+# x = range(1, 101)
+# y1 = [random.randint(1, 100) for _ in range(len(x))]
+# y2 = [random.randint(1, 100) for _ in range(len(x))]
+#
+# fig = plt.figure()
+# ax1 = fig.add_subplot(211)
+# ax2 = fig.add_subplot(212)
+#
+# .ax1.loglog(x, y1)
+# ax2.loglog(x, y2)
+#
+# # Set common labels
+# #fig.text(0.5, 0.04, 'common xlabel', ha='center', va='center')
+# #fig.text(0.06, 0.5, 'common ylabel', ha='center', va='center', rotation='vertical')
+#
+# ax1.set_title('ax1 title')
+# ax1.set_xlabel('ax1knelk')
+# ax2.set_title('ax2 title')
+# ax2.set_xlabel('rgkjrnge')
+#
+# plt.show()
--- simulate.py
+++ simulate.py
@@ -1,37 +1,34 @@
-import datetime
 import random as rnd
 from aquarium import Aquarium
-from mosquito import Mosquito
 import matplotlib.pyplot as plt
 import datetime
 
-male_gene_percentage = 30
-initial_mosquito = 10
+ls_graph_result = []
+ls_graph_result_mosquito = []
+male_gene_percentage = 0
+initial_mosquito = 50
 # the percentage of males with the "male gene"
-on = True
-def the_simulator(run):
+
+def the_simulator(run_num):
     day_counter = 1
     # in days
     simulation_length = 150
-    male_gene_percentage1 = male_gene_percentage
-    initial_mosquito1 = initial_mosquito
+    max_mosquito = 0
     # creating aqu'arium
     aquarium = Aquarium()
-    aquarium.add_mosquito(initial_mosquito1, male_gene_percentage1)
+    aquarium.add_mosquito(initial_mosquito, male_gene_percentage)
     print(aquarium)
-
+    print(male_gene_percentage)
 
     # starting the simulation
     total_mosquito_list = []
     total_female = []
     day_counter_ls = []
     l_ratio_male_gene = []
-    previous_day_amount = 0
     while day_counter <= simulation_length and len(aquarium.get_all_mosquito()) != 0:
         print("day number", day_counter)
         # Initialise the subplot function using number of rows and columns
 
-        # loop through all mosquitoes in the aquarium and connect between them
         mosquito_list = aquarium.get_all_mosquito()
         newborn_mosquito = []
         for mosquito in mosquito_list:
@@ -40,7 +37,7 @@ def the_simulator(run):
             aquarium.add_mosquitos_list(newborn_mosquito)
 
             mosquito.day_pass()
-            if mosquito.life_span == 0:
+            if mosquito.life_span <= 0:
                 mosquito_list.remove(mosquito)
                 aquarium.reduce_counters(mosquito)
         if day_counter % 5 == 0:
@@ -51,66 +48,56 @@ def the_simulator(run):
         if aquarium.total_male > 0:
             ratio = aquarium.total_male_gene / aquarium.total_male
         l_ratio_male_gene.append(ratio)
-        if day_counter>1:
-            growth_ratio = len(total_mosquito_list)/previous_day_amount
         day_counter_ls.append(day_counter)
+        if max_mosquito < len(aquarium.l_mosquito):
+            max_mosquito = len(aquarium.l_mosquito)
         day_counter += 1
-        previous_day_amount = len(total_mosquito_list)
-    # figure, axis = plt.subplots(1, 2)
-    # figure = run
-    # axis[0].plot(day_counter, aquarium.get_total_mosquito(), 'b.')
+    ls_graph_result_mosquito.append(max_mosquito)
+    ls_graph_result.append({'x': day_counter_ls,'y': total_mosquito_list})
+    # graph 1
+    plt.figure(str(run_num)+'-1')
 
-    # axis[0].set_title(my_title, fontsize=18, color='blue')
-    if on:
-        # graph 1
-        plt.figure(str(run)+'-1')
-
-        plt.plot(day_counter_ls, total_mosquito_list, 'b.')
-        plt.plot(day_counter_ls, total_female, 'm.')
-        plt.xlabel('day number', fontsize=12)
-        plt.ylabel('total mosquitoes couter', fontsize=12)
-        my_title = 'total mosquito (init =' + str(initial_mosquito1), 'male gene per =' + str(male_gene_percentage1)
-        plt.title(my_title)
-        plt.grid(True)
-
-        date = datetime.datetime.now()
-        plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito1) + '-per-' + str(male_gene_percentage1) + '-1.png',dpi = 300)
-
-        # graph 2
-        plt.figure(str(run)+'-2')
-
-        plt.plot(day_counter_ls, l_ratio_male_gene, 'b.')
-        plt.xlabel('day number', fontsize=12)
-        plt.ylabel('ratio', fontsize=12)
-        plt.ylim((male_gene_percentage1/100)-0.25,1)
-        my_title = 'ratio between mosquito with gene to all male (init =' + str(initial_mosquito1), 'male gene per =' + str(male_gene_percentage)
-        plt.title(my_title)
-        plt.grid(True)
-
-        date = datetime.datetime.now()
-        plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito1) + '-per-' + str(male_gene_percentage1) + '-2.png',
-                    dpi=300)
-
-    # graph 3
-    plt.figure(str(run) + '-3')
-
-    plt.plot(l_ratio_male_gene, day_counter_ls, 'b.')
-    plt.xlabel('male gene percentage', fontsize=12)
-    plt.ylabel('days took to obliterate', fontsize=12)
-    my_title = 'the effect of the male gene percentage on the amount of days that it takes the mosquitoes to obliterate (init =' + str(initial_mosquito1), 'male gene per =' + str(
-        male_gene_percentage1)
+    plt.plot(day_counter_ls, total_mosquito_list, 'b.')
+    plt.plot(day_counter_ls, total_female, 'm.')
+    plt.xlabel('day number', fontsize=12)
+    plt.ylabel('total mosquitoes couter', fontsize=12)
+    my_title = 'total mosquito (init =' + str(initial_mosquito), 'male gene per =' + str(male_gene_percentage)
     plt.title(my_title)
     plt.grid(True)
 
     date = datetime.datetime.now()
-    plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito1) + '-per-' + str(male_gene_percentage1) + '-3.png',
+    plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito) + '-per-' + str(male_gene_percentage) + '-1.png',dpi = 300)
+
+    # graph 2
+    plt.figure(str(run_num)+'-2')
+
+    plt.plot(day_counter_ls, l_ratio_male_gene, 'b.')
+    plt.xlabel('day number', fontsize=12)
+    plt.ylabel('ratio', fontsize=12)
+    plt.ylim((male_gene_percentage/100)-0.25,1)
+    my_title = 'ratio between mosquito with gene to all male (init =' + str(initial_mosquito), 'male gene per =' + str(male_gene_percentage)
+    plt.title(my_title)
+    plt.grid(True)
+
+    date = datetime.datetime.now()
+    plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito) + '-per-' + str(male_gene_percentage) + '-2.png',
                 dpi=300)
-
-for i in range (3):
-    if not on:
-        the_simulator(i)
-    else:
-        initial_mosquito += 10
-        the_simulator(i)
+    plt.close()
 
 
+for i in range (1):
+    the_simulator(i)
+
+
+plt.figure('summary')
+plt.hist(ls_graph_result_mosquito, )
+plt.xlabel('max mosquitoes', fontsize=12)
+plt.ylabel('amount of simulations', fontsize=12)
+my_title = 'max mosquitoes per simulation (init =' + str(initial_mosquito), 'male gene per =' + str(male_gene_percentage)
+plt.title(my_title)
+plt.grid(True)
+
+date = datetime.datetime.now()
+plt.savefig('data/' + str(date) + '-init-' + str(initial_mosquito) + '-per-' + str(male_gene_percentage) + '-3.png',
+            dpi=300)
+print(ls_graph_result_mosquito)
